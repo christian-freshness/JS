05a42aee964cb22b03476e977b2936fb82844117
christian <christianajulor@gmail.com>
2022-06-21 20:11:43 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let Name = 'Ajulor Christian'
+let height = '5.11ft'
+let country = 'Nigeria'
+
+alert(`My Name is ${Name}, I am ${height} Tall, I am from ${country}`)
+console.log(`My Name is ${Name}, I am ${height} Tall, I am from ${country}`)
+document.write(`My Name is ${Name}, I am ${height} Tall, I am from ${country}`)
